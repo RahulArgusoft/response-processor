d1eddeae2b7356c0c834ba02260837f1ffe894fb
Rahul Kumar <rkumar@argusoft.com>
2026-01-16 13:05:53 +0530

    updated database configuration

--- src/modules/common/config/database.config.ts
+++ src/modules/common/config/database.config.ts
@@ -2,10 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
     url: process.env.DATABASE_URL,
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    name: process.env.DB_NAME || 'response_processor',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '',
-    ssl: process.env.DB_SSL === 'true',
 }));
